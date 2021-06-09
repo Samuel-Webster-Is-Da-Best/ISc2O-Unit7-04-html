145231492d3c54305393f45ac4522ddfa7a499bc
SamuelWebsterIs <84517284+Samuel-Webster-Is-Da-Best@users.noreply.github.com>
2021-06-09 14:52:31 +0000

    stuff

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 document.getElementById('button').addEventListener('click', disc)
 
 function disc () {
+  //read input
   const age = parseInt(document.getElementById('inp').value)
   const day = document.getElementById('inp2').value
+  //determine if they are elegible
   if (day === 'thursday' || day === 'tuesday') {
     document.getElementById('ans').innerHTML = 'You are elegible for student pricing!'
   } else if (age > 12 && age < 21) {
