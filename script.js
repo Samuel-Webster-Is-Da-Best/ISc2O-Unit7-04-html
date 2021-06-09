c7414cebac69f7f59d4a806957f346df6e0e4876
SamuelWebsterIs <84517284+Samuel-Webster-Is-Da-Best@users.noreply.github.com>
2021-06-09 14:49:37 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById('button').addEventListener('click', disc)
+
+function disc () {
+  const age = parseInt(document.getElementById('inp').value)
+  const day = document.getElementById('inp2').value
+  if (day === 'thursday' || day === 'tuesday') {
+    document.getElementById('ans').innerHTML = 'You are elegible for student pricing!'
+  } else if (age > 12 && age < 21) {
+    document.getElementById('ans').innerHTML = 'You are elegible for student pricing!'
+  } else {
+    document.getElementById('ans').innerHTML = 'You must pay normal prices'
+  }
+}
